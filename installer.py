830a41b4ec993304353fee7a38ec7d91e654501c
hisham waleed karam <hisham.karam@cartologic.com>
2017-09-18 00:38:36 +0200

    fix installer.py

--- installer.py
+++ installer.py
@@ -1,8 +1,8 @@
 __author__ = 'cartologic'
 
 info = {
-    "title": “GeoCollect”,
-    "description": “collect XY Location and associate Attributes and Images. Work Field Crews Can utilize this Application To Collect Information About Assets .Users can also use this Application To report Issues or Collect New Features . once the data is Saved it Will Be directly Saved To the server, to setup your app author a new shapefile then upload it to the server then follow the instructions, users will use the app to save data to this layer”,
+    "title": "GeoCollect",
+    "description": "collect XY Location and associate Attributes and Images. Work Field Crews Can utilize this Application To Collect Information About Assets .Users can also use this Application To report Issues or Collect New Features . once the data is Saved it Will Be directly Saved To the server, to setup your app author a new shapefile then upload it to the server then follow the instructions, users will use the app to save data to this layer",
     "author": 'Cartologic',
     "home_page": 'http://cartoview.org/apps/test',
     "help_url": "http://cartoview.org/apps/test/help/",
